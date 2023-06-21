2fc33849ba6b8cc4aa4a26a8e3a3c34a472d561d
Bruno Almeida de Matos <brunoalmeidadem@gmail.com>
2023-06-21 00:29:16 -0300

    add AdvancedSearchField

---
+++ lib/src/ui/molecules/advanced_search_field.dart
@@ -0,0 +1,115 @@
+import 'package:coffee_cup/coffe_cup.dart';
+import 'package:flutter/material.dart';
+import 'package:manga_easy_themes/manga_easy_themes.dart';
+
+class AdvancedSearchField extends StatefulWidget {
+  final TextEditingController controller;
+  final int maxHistoryQty;
+  final List<String> list;
+  const AdvancedSearchField({
+    Key? key,
+    required this.controller,
+    this.maxHistoryQty = 7,
+    required this.list,
+  }) : super(key: key);
+
+  @override
+  State<AdvancedSearchField> createState() => _AdvancedSearchFieldState();
+}
+
+class _AdvancedSearchFieldState extends State<AdvancedSearchField> {
+  bool tappad = false;
+  String advancedHintText = 'Pesquise seus mangás favoritos';
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: ThemeService.of.selectColor,
+        borderRadius: ThemeService.of.borderRadius,
+      ),
+      alignment: Alignment.topCenter,
+      child: SingleChildScrollView(
+        physics: NeverScrollableScrollPhysics(),
+        child: Column(
+          children: [
+            NewCoffeeSearchField(
+              hintText: advancedHintText,
+              onTap: () {
+                setState(() {
+                  tappad = !tappad;
+                });
+              },
+              controller: widget.controller,
+            ),
+            Visibility(
+              visible: tappad && widget.list.isNotEmpty,
+              child: Column(
+                children: [
+                  ListView.builder(
+                    physics: NeverScrollableScrollPhysics(),
+                    shrinkWrap: true,
+                    itemCount: widget.list.length <= widget.maxHistoryQty
+                        ? widget.list.length
+                        : widget.maxHistoryQty,
+                    itemBuilder: (context, index) => Padding(
+                      padding: const EdgeInsets.fromLTRB(16, 0, 16, 8),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Row(
+                            children: [
+                              Icon(
+                                Icons.refresh,
+                                size: 24,
+                              ),
+                              const SizedBox(
+                                width: 8,
+                              ),
+                              CoffeeText(
+                                text: widget.list[index],
+                              ),
+                            ],
+                          ),
+                          CoffeeIconButton(
+                            onTap: () {
+                              setState(() {
+                                widget.list.removeAt(index);
+                              });
+                            },
+                            size: 24,
+                            icon: Icons.delete,
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                  Align(
+                    alignment: Alignment.bottomRight,
+                    child: SizedBox(
+                      width: 80,
+                      child: CoffeeButtonText(
+                        text: 'Limpar',
+                        color: ThemeService.of.backgroundIcon,
+                        onPressed: () {
+                          advancedHintText = 'Pesquise seus mangás favoritos';
+                          setState(() {
+                            widget.list.removeWhere((element) => true);
+                            tappad = false;
+                          });
+                        },
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/src/ui/pages/components_page_mobile.dart
+++ lib/src/ui/pages/components_page_mobile.dart
@@ -1,5 +1,6 @@
 import 'package:coffee_cup/coffe_cup.dart';
 import 'package:flutter/material.dart';
+import 'package:manga_easy_components/src/ui/molecules/advanced_search_field.dart';
 import 'package:manga_easy_components/src/ui/molecules/coffee_button_component.dart';
 import 'package:manga_easy_components/src/ui/molecules/coffee_carousel_component.dart';
 import 'package:manga_easy_components/src/ui/molecules/coffee_dialog_component.dart';
@@ -8,7 +9,6 @@ import 'package:manga_easy_components/src/ui/molecules/coffee_image_component.da
 import 'package:manga_easy_components/src/ui/molecules/coffee_pages_component.dart';
 import 'package:manga_easy_components/src/ui/molecules/coffee_random_component.dart';
 import 'package:manga_easy_components/src/ui/molecules/coffee_text_component.dart';
-import 'package:manga_easy_themes/manga_easy_themes.dart';
 
 class ComponentsPageMobile extends StatefulWidget {
   const ComponentsPageMobile({super.key});
@@ -19,16 +19,16 @@ class ComponentsPageMobile extends StatefulWidget {
 
 class _ComponentsPageMobileState extends State<ComponentsPageMobile> {
   final advancedEC = TextEditingController();
-  String advancedHintText = 'Pesquise seus mangás favoritos';
   List<String> resultSearch = [
-    'One Piece',
-    'One Punch Man',
-    'One',
+    'One 1',
     'One 2',
-    'One 3'
+    'One 3',
+    'One 4',
+    'One 5',
+    'One 6',
+    'One 7',
+    'One 8',
   ];
-  bool tappad = false;
-  bool history = false;
 
   @override
   Widget build(BuildContext context) {
@@ -76,72 +76,7 @@ class _ComponentsPageMobileState extends State<ComponentsPageMobile> {
                 const SizedBox(
                   height: 10,
                 ),
-                AnimatedContainer(
-                  duration: Duration(milliseconds: 150),
-                  decoration: BoxDecoration(
-                    color: ThemeService.of.selectColor,
-                    borderRadius: ThemeService.of.borderRadius,
-                  ),
-                  alignment: Alignment.topCenter,
-                  height: tappad ? (resultSearch.length * 68) : 48,
-                  child: Column(
-                    children: [
-                      NewCoffeeSearchField(
-                        hintText: advancedHintText,
-                        onTap: () {
-                          setState(() {
-                            advancedHintText = '';
-                            tappad = true;
-                          });
-                        },
-                        onTapOutside: (_) {
-                          advancedHintText = 'Pesquise seus mangás favoritos';
-                          setState(() {
-                            tappad = false;
-                          });
-                        },
-                        controller: advancedEC,
-                      ),
-                      tappad
-                          ? Padding(
-                              padding: const EdgeInsets.all(18.0),
-                              child: SizedBox(
-                                height: resultSearch.length * 40,
-                                child: ListView.builder(
-                                  itemCount: resultSearch.length,
-                                  itemBuilder: (context, index) => Row(
-                                    mainAxisAlignment:
-                                        MainAxisAlignment.spaceBetween,
-                                    children: [
-                                      Row(
-                                        children: [
-                                          CoffeeIconButton(
-                                            onTap: () {},
-                                            size: 24,
-                                            icon: Icons.refresh,
-                                          ),
-                                          const SizedBox(
-                                            width: 5,
-                                          ),
-                                          CoffeeText(
-                                            text: resultSearch[index],
-                                          ),
-                                        ],
-                                      ),
-                                      CoffeeIconButton(
-                                        onTap: () {},
-                                        size: 24,
-                                        icon: Icons.delete,
-                                      )
-                                    ],
-                                  ),
-                                ),
-                              ),
-                            )
-                          : SizedBox.shrink(),
-                    ],
-                  ),
-                ),
+                AdvancedSearchField(controller: advancedEC, list: resultSearch),
               ],
             ),
           ),
